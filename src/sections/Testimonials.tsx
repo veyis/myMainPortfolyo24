74286b045262d62d20ac3ea580285ad15308d867
veyis <vakgul@gmail.com>
2024-10-02 15:07:00 -0700

    testomanial animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -53,7 +53,7 @@ export const TestimonialsSection = () => {
 				/>
 
 				<div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4 ">
-				<div className="flex gap-8 flex-none animate-move-left [animation-duration:90s] hover:[animation-play-state:paused]">
+				<div className="flex gap-8 pr-8 flex-none animate-move-left [animation-duration:90s] hover:[animation-play-state:paused]">
 
 						{[...new Array(2)].fill(0).map((_, idx) => (
 							<Fragment key={idx}>
